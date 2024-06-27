ef862ee3c34ed523bef372be8a33b49acf84c916
PranaliR07 <154443103+PranaliR07@users.noreply.github.com>
2024-06-27 23:06:21 +0530

    DAY 2 -B (FULL ADDER)
    TO IMPLEMENT VHLD CODE FOR FULL ADDER USING STUCTURAL MODELLING.

--- DUT.vhdl
+++ DUT.vhdl
@@ -6,15 +6,15 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity DUT is
-   port(input_vector: in std_logic_vector(1 downto 0);
+   port(input_vector: in std_logic_vector(2 downto 0);
        	output_vector: out std_logic_vector(1 downto 0));
 end entity;
 
 architecture DutWrap of DUT is
 	-- Instantiate your own top Module component in place of ALU_1
 	
-component half_34 is
-port(in1,in2:in std_logic;
+component f_add is
+ port(a,b,c:in std_logic;
  sum, carry :out std_logic);
 end component;
 
@@ -22,7 +22,7 @@ begin
 
    -- input/output vector element ordering is critical,
    -- and must match the ordering in the trace file!
-   add_instance:half_34 port map (in1 => input_vector(1), in2 => input_vector(0),
+   add_instance: f_add port map (a => input_vector(2), b => input_vector(1), c => input_vector(0),
 											sum => output_vector(1), carry => output_vector(0));
 
 end DutWrap;
