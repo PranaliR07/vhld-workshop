44be7d05ce5d1744a152fe69867b360c4e561e01
PranaliR07 <154443103+PranaliR07@users.noreply.github.com>
2024-07-05 11:07:40 +0530

    DAY 3 B
    VHDL code to implement 4x1 mux using structural modelling.

--- DUT.vhdl
+++ DUT.vhdl
@@ -1,20 +1,16 @@
--- A DUT entity is used to wrap your design.
---  This example shows how you can do this for the
---  Full-adder.
-
 library ieee;
 use ieee.std_logic_1164.all;
 
 entity DUT is
-   port(input_vector: in std_logic_vector(2 downto 0);
+   port(input_vector: in std_logic_vector(5 downto 0);
        	output_vector: out std_logic_vector(0 downto 0));
 end entity;
 
 architecture DutWrap of DUT is
 	-- Instantiate your own top Module component in place of ALU_1
 	
-component mux_2x1 is
-port(in1, in0,S:in std_logic;
+component mux_4x1 is
+port(in3, in2, in1, in0, s1, s0:in std_logic;
   Y:out std_logic);
 end component;
 
@@ -22,7 +18,8 @@ begin
 
    -- input/output vector element ordering is critical,
    -- and must match the ordering in the trace file!
-   add_instance: mux_2x1 port map (in1 => input_vector(2), in0 => input_vector(1), S => input_vector(0),
-											Y => output_vector(0));
+   add_instance: mux_4x1 port map (in3 => input_vector(5), in2 => input_vector(4), in1 => input_vector(3),
+												in0 => input_vector(2),
+											s1 => input_vector(1),s0 => input_vector(0),Y => output_vector(0));
 
 end DutWrap;
